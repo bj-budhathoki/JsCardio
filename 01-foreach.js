875a5ecc627d8d268d25667f61afef4a7c0a9731
bj-budhathoki <bijayabudhathoki98@gmail.com>
2018-05-15 10:38:58 +0545

    foreach

---
+++ 01-foreach.js
@@ -0,0 +1,9 @@
+/*classic js
+var palces = ['kathmandu', 'Bhaktapur', 'Lalitpur', 'Phokhara', 'nepaljung'];
+for (var i = 0; i < palces.length; i++) {
+   console.log(palces[i]);
+}
+*/
+//ES6
+const palces = ['kathmandu', 'Bhaktapur', 'Lalitpur', 'Phokhara', 'nepaljung'];
+palces.forEach(place => console.log(place));
