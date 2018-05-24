37d27f1cb5414e08e82b0957407303de6ce0c820
bj-budhathoki <bijayabudhathoki98@gmail.com>
2018-05-24 16:53:23 +0545

    capitalize first latter

--- test.js
+++ test.js
@@ -38,5 +38,18 @@ function reverseInt(int) {
       .join('');
    return parseInt(revInt) * Math.sign(int);
 }
-const output = reverseInt(-12345);
+function capitalizeLetters(str) {
+   // const strArr = str.toLowerCase().split(' ');
+   // for (let i = 0; i < strArr.length; i++) {
+   //    strArr[i] =
+   //       strArr[i].substring(0, 1).toUpperCase() + strArr[i].substring(1);
+   // }
+   // return strArr.join(' ');
+   return str
+      .toLowerCase()
+      .split(' ')
+      .map(word => word[0].toUpperCase() + word.substring(1))
+      .join(' ');
+}
+const output = capitalizeLetters('i love javascript');
 console.log(output);
